2001bd8cdd89f3ec47a74932186dc24341cac119
jihunJeong <jihun2729@gmail.com>
2020-02-18 12:32:40 +0900

    Solve 18 Feb 20

--- StudyGroup/2020_02_02/1060.py
+++ StudyGroup/2020_02_02/1060.py
@@ -1,39 +1,34 @@
 import sys
 import heapq
+import math
 
 L = int(sys.stdin.readline())
 input_list = list(map(int, sys.stdin.readline().split()))
 input_list.insert(0, 0)
 N = int(sys.stdin.readline())
-count = []
-cnt = {}
-dis = dict()
+count = {}
 
 for i in range(1, L + 1):
-	#count[input_list[i]] = 0
-	heapq.heapqpush(count, (0, input_list[i]))
-	if i != L and input_list[i + 1] - input_list[i] >= 2:
-		dis[i] = input_list[i + 1] - input_list[i]
-dis[0] = input_list[0]
+	count[input_list[i]] = 0
+input_list.sort()	
 
-dis = sorted(dis.items(), key=lambda item: item[1])
-for i in range(len(dis)):
-	a = dis[i][0]
-	if input_list[a + 1] - input_list[a] == 2:
-		#count[input_list[a] + 1] = 0
-		heapq.heappush(count, (0, input_list[a] + 1))
-	for j in range(input_list[a] + 1, input_list[a + 1] - 1):
-		for k in range(j + 1, input_list[a + 1]):
-			for p in range(j, k + 1):
-				if p not in count.keys():
-					cnt[p] = 1
-				else :
-					cnt[p] += 1
+for i in range(len(input_list) - 1):
+	dis = input_list[i + 1] - input_list[i] - 1
+	if dis == 1:
+		count[input_list[i] + 1] = 0
+	elif dis == 0:
+		continue
+	elif dis <= N:
+		for j in range(1, math.ceil(dis/2) + 1):
+			count[input_list[i] + j] = j * (dis + 1 - j) - 1 
+			count[input_list[i + 1] - j] = j * (dis + 1 - j) - 1
+	elif dis > N:
+		for j in range(1, math.ceil(N/2) + 1):
+			count[input_list[i] + j] = j * (dis + 1 - j) - 1
+			count[input_list[i + 1] - j] = j * (dis + 1 - j) - 1
 
-
-#count = list(sorted(count.items(), key=lambda item: (item[1], item[0])))
+count = list(sorted(count.items(), key=lambda item: (item[1], item[0])))
 for i in range(min(N, len(count))):
-	#print(count[i][0], end=" ")
-	print(heapq.heappop(count)[1], end=" ")
+	print(count[i][0], end=" ")
 for i in range(N - len(count)):
 	print(input_list[L] + i + 1, end=" ")
