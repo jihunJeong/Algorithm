5c2e85cfdf479337f511ceb45106f9f41bece1fb
jihunJeong <jihun2729@gmail.com>
2020-02-29 02:51:00 +0900

    solve 29 Feb 20

--- StudyGroup/2020_02_04/1111.py
+++ StudyGroup/2020_02_04/1111.py
@@ -1,6 +1,5 @@
 import sys
 
-"""
 def error():
 	print("B")
 	sys.exit()
@@ -8,27 +7,28 @@ def error():
 N = int(sys.stdin.readline())
 num = list(map(int, sys.stdin.readline().split()))
 
-if N == 2:
-	if num[1] - num[0] == 0:
-		print(num[0])
-		sys.exit()
 if N <= 2:
-	error()
+	if N == 2 and num[0] == num[1]:
+		print(num[0])
+	else :
+		print("A")
+	sys.exit()
 
-if num[1] - num[0] == 0:
-	a1 = 0
-else :
-	a1 = (num[2] - num[1]) // (num[1] - num[0])
+if num[0] == num[1] or num[1] == num[2]:
 	for i in range(1, N - 1):
-		if (num[i + 1] - num[i])a1 != num[i + 2] - num[i + 1]
-b1 = num[1] - num[0] * a1
-
-if a1 == 0:
-	for i in range(1, N - 1):
-		if num[i + 1] - num[i] == 0:
-			continue
-		else :
+		if num[i] != num[i + 1]:
 			error()
+	if num[0] == num[1]:
+		print(num[0])
+	else :
+		print(num[1])
 else :
-	for i in range(2, N - 1):
-"""
+	a = (num[2] - num[1]) // (num[1] - num[0]) 
+	for i in range(N - 2):
+		if num[i + 1] == num[i + 2]:
+			error()
+		a1 = (num[i + 2] - num[i + 1]) // (num[i + 1] - num[i])
+		if a != a1 or (num[i + 2] - num[i + 1]) % (num[i + 1] - num[i]) != 0:
+			error()
+	b = num[1] - num[0]*a
+	print(num[N - 1]*a + b)
