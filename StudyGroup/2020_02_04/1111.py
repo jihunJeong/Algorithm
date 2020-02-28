17aecc749cf5155b3414e5da3075869af4590a0c
jihunJeong <jihun.jeong219@gmail.com>
2020-02-28 23:51:57 +0900

    start 28 Feb 20

---
+++ StudyGroup/2020_02_04/1111.py
@@ -0,0 +1,34 @@
+import sys
+
+"""
+def error():
+	print("B")
+	sys.exit()
+
+N = int(sys.stdin.readline())
+num = list(map(int, sys.stdin.readline().split()))
+
+if N == 2:
+	if num[1] - num[0] == 0:
+		print(num[0])
+		sys.exit()
+if N <= 2:
+	error()
+
+if num[1] - num[0] == 0:
+	a1 = 0
+else :
+	a1 = (num[2] - num[1]) // (num[1] - num[0])
+	for i in range(1, N - 1):
+		if (num[i + 1] - num[i])a1 != num[i + 2] - num[i + 1]
+b1 = num[1] - num[0] * a1
+
+if a1 == 0:
+	for i in range(1, N - 1):
+		if num[i + 1] - num[i] == 0:
+			continue
+		else :
+			error()
+else :
+	for i in range(2, N - 1):
+"""
