4145e79aeb9083977cec25d0bdccdb8d71b13fb8
jihunJeong <jihun2729@gmail.com>
2020-03-10 23:35:19 +0900

    solve 10 Mar 20

--- StudyGroup/2020_03_01/2042.py
+++ StudyGroup/2020_03_01/2042.py
@@ -1,15 +1,14 @@
-import math
+import sys
 
 def init(node, start, end):
 	if start == end:
 		tree[node] = num[start]
-		return tree[node]
 	else :
 		tree[node] = init(node*2, start, (start+end)//2) + init(node*2+1, (start+end)//2+1, end)
-		return tree[node]
+	return tree[node]
 
 def sum(node, start, end, left, right):
-	if left > end or right < end:
+	if left > end or right < start:
 		return 0
 	if left <= start and end <= right:
 		return tree[node]
@@ -17,8 +16,8 @@ def sum(node, start, end, left, right):
 	return ret
 
 def update(node, start, end, index, diff):
-	if index < start or end < index:
-		return 
+	if index < start or index > end:
+		return 0
 	tree[node] = tree[node] + diff
 	if start != end:
 		update(node*2, start, (start+end)//2, index, diff)
@@ -30,15 +29,12 @@ tree = [0] * (N * 4)
 for _ in range(N):
 	num.append(int(input()))
 
-
-
 init(1, 0, N-1)
 for i in range(M + K):
 	a, b, c = map(int, input().split())
 	if a == 1:
-		diff = c - num[b - 1]
+		diff = c - num[b-1]
 		num[b-1] = c
 		update(1, 0, N-1, b-1, diff)
-		print(tree)
 	else :
 		print(sum(1, 0, N-1, b-1, c-1))
