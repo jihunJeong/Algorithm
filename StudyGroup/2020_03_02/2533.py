a0a868126e9bfdf998d89189c210db08f0b86103
jihunJeong <jihun2729@gmail.com>
2020-03-31 19:46:36 +0900

    solve

--- StudyGroup/2020_03_02/2533.py
+++ StudyGroup/2020_03_02/2533.py
@@ -3,6 +3,8 @@ import sys
 sys.setrecursionlimit(10**6)
 
 def dfs(index):
+	if visited[index] == 1:
+		return 0
 	visited[index] = 1
 	dp[index][0] = 0
 	dp[index][1] = 1
@@ -24,5 +26,4 @@ for i in range(N-1):
 dp = [[0 for x in range(2)] for y in range(N+1)]
 visited = [0] * (N+1)
 dfs(1)
-print(dp)
 print(min(dp[1][0], dp[1][1]))
